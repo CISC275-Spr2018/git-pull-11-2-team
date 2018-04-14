a1a712af57b05b44586ef4cf1e5fcfc8a538e6ab
Zack Klodnicki <klodzack@udel.edu>
2018-04-13 23:59:43 -0400

    Reorg and loadImg and splitIntoTiles
    Moved temporary code that should have been in subclasses actually into subclasses.
    Added a convenience function loadImg
    Added a convenience function splitIntoTiles
    Added some comments

--- src/Animation.java
+++ src/Animation.java
@@ -3,22 +3,53 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+// This class contains useful public static final Animations.
+// You cannot create additional Animations in other files; they should all be here.
+// The only public method of an Animation is getCurrentFrameForDirection(d)
+
 public abstract class Animation {
-	public static final Animation WALKING = new Animation() {};
+	public static final Animation WALKING = new Animation() {
+		private BufferedImage[] east;
 
-	private BufferedImage bi;
+		protected void load() {
+			east = splitTiled(loadImg("orc_forward_east"), 10, 1);
+		}
 
+		public BufferedImage getCurrentFrameForDirection(Direction d) {
+			return east[0];
+		}
+	};
+
+	// All subclasses must implement these methods.
+	protected abstract void load();
+	public abstract BufferedImage getCurrentFrameForDirection(Direction d);
+
+	// Will always load on construction.
 	private Animation() {
-		// TEMP for testing
+		this.load();
+	} // Prevent instantiation
+
+	//---------- Convenience methods for inside subclasses:
+
+	// loadImg(String name): Basically a convenience wrapper around ImageIO.read().
+	protected BufferedImage loadImg(String name) {
 		try {
-			bi = ImageIO.read(new File("images/orc/orc_forward_east.png"));
+			return ImageIO.read(new File("images/orc/"+name+".png"));
 		} catch(IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-	} // Prevent instantiation
+		return null;
+	}
 
-	public BufferedImage getCurrentFrameForDirection(Direction d) {
-		return bi;
+	protected BufferedImage[] splitTiled(BufferedImage source, int xframes, int yframes) {
+		BufferedImage[] result = new BufferedImage[xframes*yframes];
+		int frameW = source.getWidth()/xframes;
+		int frameH = source.getHeight()/yframes;
+		for(int x=0; x<xframes; x++)
+			for(int y=0; y<yframes; y++)
+				result[x+xframes*y] = source.getSubimage(
+					x*frameW, y*frameH, frameW, frameH);
+		return result;
 	}
 }
